70bd5c473e7497ade5a788fa84aa01ad62b14269
leonardo <leonarrangel@gmail.com>
2017-04-14 23:06:22 -0300

    add index page and routes

--- app/routes/a-estrela.js
+++ app/routes/a-estrela.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
   app.get('/a-estrela', function(req, res) {
     res.render('a-estrela');
   });
+
+  app.get('/', function(req, res) {
+    res.render('index');
+  });
 }
