a81ecc08059081eafc67a07682e7c74b84b63ca2
leonardo <leonarrangel@gmail.com>
2017-04-16 20:44:07 -0300

    Add json request to initial marks

--- public/js/site/algoritmo-a-estrela.js
+++ public/js/site/algoritmo-a-estrela.js
@@ -1,3 +1,10 @@
 var trocaStatusMarca = function(marker) {
   console.log(marker);
 }
+
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
