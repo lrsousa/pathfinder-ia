412561686b3f9fc0fadcf44c05c2ea81cfc0595c
leonardo <leonarrangel@gmail.com>
2017-04-17 00:13:59 -0300

    turn array object into json object

--- public/js/site/algoritmo-a-estrela.js
+++ public/js/site/algoritmo-a-estrela.js
@@ -1,10 +1,34 @@
+var bkpPontos = [];
+
+var ponto = function(objeto) {
+  return {
+    id : objeto.id,
+    lat: objeto.lat,
+    lng: objeto.lng,
+    vizinhos: objeto.vizinhos,
+    visitado: false,
+    ativo: true,
+    pai: null,
+    distanciaGeodesicaDestino: function(destino) {
+        return google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(this.lat, this.lng), new google.maps.LatLng(destino.lat, destino.lng));
+    },
+    print: function(){
+      console.log("id: "+ this.id+",  lat é " + this.lat + ", long é " + this.lng);
+    }
+  };
+};
+
+var montarListaDePontos = function(pontos) {
+  // bkpPontos.push();
+};
+
 var trocaStatusMarca = function(marker) {
   console.log(marker);
-}
+};
 
 function Get(yourUrl){
     var Httpreq = new XMLHttpRequest();
     Httpreq.open("GET",yourUrl,false);
     Httpreq.send(null);
     return Httpreq.responseText;
-}
+};
