f913e7fc5a4cacefda9617bfb12661591912eade
leonardo <leonarrangel@gmail.com>
2017-04-20 08:20:37 -0300

    add pathfinder

--- public/js/site/algoritmo-a-estrela.js
+++ public/js/site/algoritmo-a-estrela.js
@@ -1,4 +1,3 @@
-
 var ponto = function(objeto) {
   return {
     id : objeto.id,
@@ -7,9 +6,10 @@ var ponto = function(objeto) {
     vizinhos: objeto.vizinhos,
     visitado: false,
     ativo: true,
+    distanciaAcumulada: 0,
     pai: null,
-    distanciaGeodesicaDestino: function(destino) {
-        return google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(this.lat, this.lng), new google.maps.LatLng(destino.lat, destino.lng));
+    distanciaGeodesicaDestino: function(d) {
+        return google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(this.lat, this.lng), new google.maps.LatLng(d.lat, d.lng));
     },
     print: function(){
       console.log("id: "+ this.id+",  lat é " + this.lat + ", long é " + this.lng);
@@ -30,4 +30,33 @@ var Get = function(yourUrl){
     Httpreq.open("GET",yourUrl,false);
     Httpreq.send(null);
     return Httpreq.responseText;
+};
+
+var listaAberta = [];
+var listaFechada = [];
+
+var pathfinder = function() {
+  origem.visitado = true;
+  listaAberta.push(origem);
+  while (origem.id != destino.id) {
+    for (var i = 0; i < origem.vizinhos.length; i++) {
+      if(pontos[origem.vizinhos[i] - 1].ativo && listaAberta.indexOf(origem.vizinhos[i]) < 0) {
+        pontos[origem.vizinhos[i] - 1].pai = origem;
+        pontos[origem.vizinhos[i] - 1].distanciaAcumulada = pontos[origem.vizinhos[i] - 1].distanciaGeodesicaDestino(origem) + origem.distanciaAcumulada;
+        listaAberta.push(pontos[origem.vizinhos[i] - 1]);
+      }
+    }
+    console.log(listaAberta);
+    listaFechada.push(listaAberta.shift());
+    listaAberta.sort(comparaDistancia);
+    origem = listaAberta.shift();
+    listaFechada.push(origem);
+  }
+  listaFechada.push(listaAberta.shift());
+};
+// calcular distancia caminhada
+// https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=-30.0333198,-51.2315908&destinations=-30.0317965,-51.2313762&mode=walking&key=AIzaSyBrPzOscJfdLUzjF7tBZyFVaQth4sAGUO0
+
+var comparaDistancia = function(a, b) {
+  return (a.distanciaAcumulada + a.distanciaGeodesicaDestino(destino)) - (b.distanciaAcumulada + b.distanciaGeodesicaDestino(destino));
 };
