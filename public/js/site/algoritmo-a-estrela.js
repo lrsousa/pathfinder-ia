26bef5513cd9c05afded1a3597770349afd5cc6c
leonardo <leonarrangel@gmail.com>
2017-04-22 01:43:54 -0300

    pathfinder not finished

--- public/js/site/algoritmo-a-estrela.js
+++ public/js/site/algoritmo-a-estrela.js
@@ -6,7 +6,9 @@ var ponto = function(objeto) {
     vizinhos: objeto.vizinhos,
     visitado: false,
     ativo: true,
-    distanciaAcumulada: 0,
+    f: 0,
+    g: 0,
+    h: 0,
     pai: null,
     distanciaGeodesicaDestino: function(d) {
         return google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(this.lat, this.lng), new google.maps.LatLng(d.lat, d.lng));
@@ -36,27 +38,45 @@ var listaAberta = [];
 var listaFechada = [];
 
 var pathfinder = function() {
-  origem.visitado = true;
+  listaAberta = [];
+  listaFechada = [];
+
   listaAberta.push(origem);
   while (origem.id != destino.id) {
-    for (var i = 0; i < origem.vizinhos.length; i++) {
-      if(pontos[origem.vizinhos[i] - 1].ativo && listaAberta.indexOf(origem.vizinhos[i]) < 0) {
-        pontos[origem.vizinhos[i] - 1].pai = origem;
-        pontos[origem.vizinhos[i] - 1].distanciaAcumulada = pontos[origem.vizinhos[i] - 1].distanciaGeodesicaDestino(origem) + origem.distanciaAcumulada;
-        listaAberta.push(pontos[origem.vizinhos[i] - 1]);
-      }
-    }
-    console.log(listaAberta);
-    listaFechada.push(listaAberta.shift());
     listaAberta.sort(comparaDistancia);
     origem = listaAberta.shift();
     listaFechada.push(origem);
+
+    for (var i = 0; i < origem.vizinhos.length; i++) {
+      var pontoI = pontos[origem.vizinhos[i] - 1];
+      if(pontoI.ativo && listaFechada.indexOf(pontoI) < 0) {
+          if(listaAberta.indexOf(pontoI) < 0) {
+            listaAberta.push(pontoI);
+            pontoI.pai = origem;
+            pontoI.g = pontoI.distanciaGeodesicaDestino(origem) + origem.g;
+            pontoI.h = pontoI.distanciaGeodesicaDestino(destino);
+            pontoI.f = pontoI.g + pontoI.h;
+          } else {
+            if(origem.g < pontoI.g) {
+              pontoI.pai = origem;
+              pontoI.g = pontoI.distanciaGeodesicaDestino(origem) + origem.g;
+              pontoI.f = pontoI.g + pontoI.h;
+            }
+          }
+      }
+    }
   }
   listaFechada.push(listaAberta.shift());
+
+  var opa = destino;
+  while(opa != null) {
+    console.log(opa.id);
+    opa = opa.pai;
+  }
 };
 // calcular distancia caminhada
 // https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=-30.0333198,-51.2315908&destinations=-30.0317965,-51.2313762&mode=walking&key=AIzaSyBrPzOscJfdLUzjF7tBZyFVaQth4sAGUO0
 
 var comparaDistancia = function(a, b) {
-  return (a.distanciaAcumulada + a.distanciaGeodesicaDestino(destino)) - (b.distanciaAcumulada + b.distanciaGeodesicaDestino(destino));
+  return (a.f - b.f);
 };
